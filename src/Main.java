6ea84a107f5574c6729bf18e10d1ee4530bb8235
ElenaGolovataya <golovataya.e@yandex.ru>
2022-09-01 18:49:18 +0300

    Eraser commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +10,12 @@ public class Main {
                 new Person("Lena", "Petrova-Vodkina", 12),
                 new Person("Svetlana", "Mariinova-Tolstova-Mechnik", 16),
                 new Person("Sergey", "Luchev-Vetlitskiy", 13),
+                new Person("Mikhail", "Veschev-Ryinochkin", 18),
+                new Person("Valeriy", "Obramovich", 20),
                 new Person("Ivan", "Zhvanetskiy", 13)
         ));
+        Predicate<Person> predicate = person -> person.getAge() < 18;
+        notables.removeIf(predicate);
         Collections.sort(notables, new ComparePerson());
         System.out.println(notables);
     }
